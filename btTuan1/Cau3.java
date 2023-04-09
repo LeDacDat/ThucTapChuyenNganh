3dd8d61ac3f56c9f7b99116091cb5fe5c1db6f98
David Lee <ledacdat03@gmail.com>
2023-04-09 14:31:15 +0700

    update lan 4

---
+++ btTuan1/Cau3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Cau3 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a String:");
+		Scanner scan=new Scanner (System.in);
+		String text= scan.next();
+		System.out.println(text.trim());
+	}
+
+}
